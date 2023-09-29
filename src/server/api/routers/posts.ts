27241f9122e4ee3c0f0d6066d11931afefef17e1
Alex Cheng <alexcheng0314@gmail.com>
2023-09-29 16:26:45 +0200

    a

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -30,7 +30,7 @@ export const postsRouter = createTRPCRouter({
     return posts.map((post) => {
       const author = users.find((user) => user.id === post.authorId);
 
-      if (!author || !author.name)
+      if (!author)
         throw new TRPCError({
           code: "INTERNAL_SERVER_ERROR",
           message: "Author for post not found",
